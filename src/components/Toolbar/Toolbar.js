c6f04ce8e061b85ed556c9023bff74369e5d50c2
Žygimantas <zyg.sid2@gmail.com>
2018-11-14 12:44:43 +0200

    Pataisytas logo ir spalvos navbaro

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './Toolbar.css';
-import logo from './a'
+import logo from '../img/logo.png'
 
 const toolbar = props => (
   <header className="toolbar">
@@ -11,13 +11,13 @@ const toolbar = props => (
         </div>
         <div className="toolbar__logo">
 <a href="/">
-<img src={logo} height="56" width="150" alt="Nutri logo" /></a>
+<img src={logo} className="navbarLogo" alt="Nutri logo" /></a>
 </div>
         <div className="spacer" />
         <div className="toolbar_navigation-items">
             <ul>
-                <li><a href="/">Produktai</a></li>
-                <li><a href="/">Apie</a></li>
+                <li><a href="#">Produktai</a></li>
+                <li><a href="#">Receptai</a></li>
             </ul>
         </div>
     </nav>
